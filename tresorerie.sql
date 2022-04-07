da04231753335119899c402db253070769b09cb7
Rudy Ceroli <39409203+Rudy2612@users.noreply.github.com>
2022-04-07 09:40:01 +0200

    Add file

---
+++ tresorerie.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 07 avr. 2022 à 07:39
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `b_fredi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tresorerie`
+--
+-- Erreur de lecture de structure pour la table b_fredi.tresorerie : #1146 - La table 'b_fredi.tresorerie' n'existe pas
+-- Erreur de lecture des données pour la table b_fredi.tresorerie : #1064 - Erreur de syntaxe près de 'FROM `b_fredi`.`tresorerie`' à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
